131c6017d01f20daa9c142d777e6602c57fc767c
AndrejsKrilovs <andrejka1990@gmail.com>
2019-11-02 00:22:35 +0200

    Created integration tests for REST-API

--- src/test/java/ru/krilovs/andrejs/insuranceapi/api/PolicyServiceTest.java
+++ src/test/java/ru/krilovs/andrejs/insuranceapi/api/PolicyServiceTest.java
@@ -1,36 +1,93 @@
 package ru.krilovs.andrejs.insuranceapi.api;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import ru.krilovs.andrejs.insuranceapi.entity.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(value = SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class PolicyServiceTest {
-    @Autowired
-    private MockMvc mockMvc;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private Policy policy;
+    private PolicyObject policyObject;
+    private PolicySubObject policySubObject1;
+    private PolicySubObject policySubObject2;
 
-    @Autowired
-    private PolicyController policyController;
+    @Before
+    public void init() {
+        policy = new Policy();
+        policyObject = new PolicyObject();
+        policySubObject1 = new PolicySubObject();
+        policySubObject2 = new PolicySubObject();
+
+        policySubObject1.setName("Sub object 1");
+        policySubObject1.setInsuredSum(Double.valueOf(45.01));
+        policySubObject1.setRiskType(RiskType.FIRE);
+
+        policySubObject2.setName("Sub object 1");
+        policySubObject2.setInsuredSum(Double.valueOf(54.99));
+        policySubObject2.setRiskType(RiskType.WATER);
+
+        policyObject.setName("Object");
+        policyObject.setSubObjects(List.of(policySubObject1, policySubObject2));
+
+        policy.setUserName("Test user");
+        policy.setPolicyStatus(Status.APPROVE);
+        policy.setPolicyObjects(List.of(policyObject));
+    }
 
     @Test
-    public void testContentType() throws Exception {
-        this.mockMvc.perform(get("/policy"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    public void postPolicyTest() {
+        final ResponseEntity<String> result = restTemplate
+                .postForEntity("http://localhost:8080/policy", policy, String.class);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
 
+    @Test
+    public void putPoliceTest() {
+        policy.setUserName("Changed user");
+        policySubObject2.setInsuredSum(Double.valueOf(100));
+        policySubObject1.setRiskType(RiskType.FIRE);
+        final ResponseEntity<String> result = restTemplate
+                .exchange(
+                        "http://localhost:8080/policy/LV19-07-100000-1",
+                        HttpMethod.PUT,
+                        new HttpEntity<>(policy),
+                        String.class
+                );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    public void getAllPoliciesTest() {
+        final ResponseEntity<Policy[]> result = restTemplate
+                .getForEntity("http://localhost:8080/policy", Policy[].class);
+        assertTrue(result.getBody().length > 0);
+    }
+
+    @Test
+    public void deleteCurrentPolicy() {
+        final ResponseEntity<String> result = restTemplate
+                .exchange(
+                        "http://localhost:8080/policy/LV19-07-100000-1",
+                        HttpMethod.DELETE,
+                        new HttpEntity<>(policy),
+                        String.class
+                );
+        assertEquals(HttpStatus.OK, result.getStatusCode());
     }
 }
